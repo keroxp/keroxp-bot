d013408dffc62dbc740037de5db3cbbe0501d8d0
Yusuke Sakurai <kerokerokerop@gmail.com>
2016-06-05 19:05:49 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/keroxp-bot-0.0.1.jar
---
+++ src/main/kotlin/me/keroxp/bot/Application.kt
@@ -0,0 +1,19 @@
+package me.keroxp.bot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@SpringBootApplication
+@Controller
+open class Application : SpringApplication() {
+    @RequestMapping("/hello")
+    @ResponseBody
+    fun hello(): String = "hello, kotlin!"
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
